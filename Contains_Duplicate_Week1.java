d73c11299a2da3a82ec08edd87ba9b25cf8fdf41
Jaya Sabarish Reddy Remala <64861603+sabarishreddy99@users.noreply.github.com>
2024-03-04 23:05:22 -0500

    Update Contains_Duplicate_Week1.java

--- Contains_Duplicate_Week1.java
+++ Contains_Duplicate_Week1.java
@@ -1,1 +1,27 @@
+class Solution {
+    public boolean containsDuplicate(int[] arr) {
 
+        for(int i=0;i<arr.length-1;i++){
+            int j=i+1;
+            while(i>=0 && arr[i]>=arr[j]){
+                if(arr[i]==arr[j]){
+                    swap(arr,i,j);
+                    return true;
+                }
+                swap(arr,i,j);
+                i--;
+                j--;
+            }
+        }
+        
+        System.out.println(Arrays.toString(arr));
+        return false;
+        
+    }
+
+    public void swap(int[] arr, int a, int b){
+        int temp=arr[b];
+        arr[b]=arr[a];
+        arr[a]=temp;
+    }
+}
